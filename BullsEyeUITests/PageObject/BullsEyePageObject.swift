b6eaf3705c97e2d02824491fbcd05b88ab43222b
IlyaMarkelov <marckellow@icloud.com>
2021-10-25 20:30:03 +0300

    Initial Commit

---
+++ BullsEyeUITests/PageObject/BullsEyePageObject.swift
@@ -0,0 +1,47 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import XCTest
+
+class BullsEyePageObject {
+  
+  var app: XCUIElement { XCUIApplication() }
+  
+  var slideButton: XCUIElement { self.app.segmentedControls.buttons["Slide"] }
+  
+  var typeButton: XCUIElement { self.app.segmentedControls.buttons["Type"] }
+  
+  var slideLabel: XCUIElement { self.app.staticTexts["Get as close as you can to: "] }
+  
+  var typeLabel: XCUIElement { self.app.staticTexts["Guess where the slider is: "] }
+  
+  var hitMeButton: XCUIElement { self.app.buttons["hit me!"] }
+
+  var alert: XCUIElement { self.app.alerts["you scored 100 points"] }
+
+}
